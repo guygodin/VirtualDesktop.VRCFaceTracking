99718d4a51e67be6a1f315429826b3e0407cb83a
regzo2 <regzo252@gmail.com>
2024-07-01 01:15:37 -0400

    Updated Quaternion to provide a normalized cartesian.

--- Quaternion.cs
+++ Quaternion.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using VRCFaceTracking.Core.Types;
 
 namespace VirtualDesktop.FaceTracking
 {
@@ -23,6 +25,22 @@ namespace VirtualDesktop.FaceTracking
             Y = y;
             Z = z;
             W = w;
+        }
+        #endregion
+
+        #region Methods
+        public Vector2 Cartesian()
+        {
+            float magnitude = (float)Math.Sqrt(X*X + Y*Y + Z*Z + W*W);
+            float Xm = X / magnitude;
+            float Ym = Y / magnitude;
+            float Zm = Z / magnitude;
+            float Wm = W / magnitude;
+
+            float pitch = (float)Math.Asin(2 * (Xm*Zm - Wm*Ym));
+            float yaw = (float)Math.Atan2(2 * (Ym*Zm + Wm*Xm), Wm*Wm - Xm*Xm - Ym*Ym + Zm*Zm);
+
+            return new Vector2(pitch, yaw);
         }
         #endregion
     }
